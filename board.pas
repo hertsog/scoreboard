728194694b291248ba8f377d20f1d7fcb848572c
hertsog <hertsog@users.noreply.github.com>
2016-10-06 23:29:24 +0300

    First piece of auto-alignment of board components

--- board.pas
+++ board.pas
@@ -5,14 +5,19 @@ unit board;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls;
 
 type
+
+  { TfrmBoard }
+
   TfrmBoard = class(TForm)
+    shpShotclock: TShape;
   private
     { private declarations }
   public
     { public declarations }
+    procedure ReAlign;
   end;
 
 var
@@ -21,6 +26,15 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TfrmBoard }
+
+procedure TfrmBoard.ReAlign;
+begin
+  shpShotclock.Height := trunc(frmBoard.Height * 0.3);
+  shpShotclock.Width := shpShotclock.Height;
+  shpShotclock.Left := (frmBoard.Width div 2) - (shpShotclock.Width div 2);
+end;
 
 end.
 
--- controlwindow.pas
+++ controlwindow.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Buttons,
-  board;
+  StdCtrls, board, windows;
 
 type
 
@@ -15,6 +15,8 @@ type
   TfrmControl = class(TForm)
     btnShowBoard: TSpeedButton;
     procedure btnShowBoardClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
     { private declarations }
   public
@@ -35,30 +37,45 @@ var
   boardScreen: Integer;
   i: Integer;
 begin
-  if Screen.MonitorCount < 2 then
-  begin
-    ShowMessage('Error: Second monitor not found');
-    exit;
-  end;
+  //if Screen.MonitorCount < 2 then
+  //begin
+  //  ShowMessage('Error: Second monitor not found');
+  //  exit;
+  //end;
 
   // Find monitor of control window
   // Show scoreboard on other monitor
-  for i := 0 to Screen.MonitorCount - 1 do
-  begin
-    if (Top >= Screen.Monitors[i].Top)
-    and (Top < Screen.Monitors[i].Top + Screen.Monitors[i].Height)
-    and (Left >= Screen.Monitors[i].Left)
-    and (Left < Screen.Monitors[i].Left + Screen.Monitors[i].Width) then
-    begin
-      boardScreen := (i + 1) mod Screen.MonitorCount;
-      frmBoard.Top := Screen.Monitors[boardScreen].Top;
-      frmBoard.Left := Screen.Monitors[boardScreen].Left;
-      frmBoard.Show;
-      frmBoard.WindowState := wsMaximized;
-      break;
-    end;
-  end;
+  //for i := 0 to Screen.MonitorCount - 1 do
+  //begin
+  //  if (Top >= Screen.Monitors[i].Top)
+  //  and (Top < Screen.Monitors[i].Top + Screen.Monitors[i].Height)
+  //  and (Left >= Screen.Monitors[i].Left)
+  //  and (Left < Screen.Monitors[i].Left + Screen.Monitors[i].Width) then
+  //  begin
+  //    boardScreen := (i + 1) mod Screen.MonitorCount;
+  //    frmBoard.Top := Screen.Monitors[boardScreen].Top;
+  //    frmBoard.Left := Screen.Monitors[boardScreen].Left;
+  //    break;
+  //  end;
+  //end;
 
+  frmBoard.Show;
+  frmBoard.BorderStyle := bsSizeable;
+  //frmBoard.WindowState := wsMaximized;
+  frmBoard.ReAlign;
+end;
+
+procedure TfrmControl.FormCreate(Sender: TObject);
+begin
+  AddFontResource('digital-7-mono.ttf');
+  SendMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
+  //Label1.Font.Name := 'Digital-7 Mono';
+end;
+
+procedure TfrmControl.FormDestroy(Sender: TObject);
+begin
+  RemoveFontResource('digital-7-mono.ttf');
+  SendMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
 end;
 
 end.
